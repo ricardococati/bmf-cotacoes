32fa0ef2df06c806214418b99fef55e542200353
João Rodrigues <ricardo.cocati@gmail.com>
2018-07-07 14:09:58 -0300

    Remoção das pastas de arquivos .class

--- bmf-cotacoes-application/target/classes/sql/cria-bancodados.sql
+++
@@ -1,9 +0,0 @@
-drop table campanha if exists;
-
-create table campanha (
-	id_campanha integer primary key,
-	time_do_coracao varchar(50),
-	nome_campanha varchar(50),
-	data_vigencia_init date,
-	data_vigencia_term date
-);
